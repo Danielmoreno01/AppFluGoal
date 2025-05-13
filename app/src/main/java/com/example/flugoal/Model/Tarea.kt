fa4ffa419411a59428a2f6d199d472bcbba9b0f0
CristianCanoIng <canox09@gmail.com>
2025-05-12 22:10:33 -0500

    jrfj

--- app/src/main/java/com/example/flugoal/Model/Tarea.kt
+++ app/src/main/java/com/example/flugoal/Model/Tarea.kt
@@ -4,7 +4,7 @@ data class Tarea(
     val id: Long? = null,
     val titulo: String,
     val descripcion: String,
-    val fechaCreacion: String, // formato ISO 8601: "yyyy-MM-dd"
+    val fechaCreacion: String, // formato ISO 8601: "yyyy-MM-dd" x
     val completada: Boolean,
     val usuario: Usuario? = null
 )
